8eab7b83c9b76efd083e1702e90d91ad8627bbd6
sambulo <sambulo.ntshalintshali@neutrinos.com>
2026-07-17 13:50:10 +0200

    Prod enc updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
-};
+  production: true,
+    stripePublishableKey: 'pk_test_51TtRnURYnZIf8ttsPIu8oEZ1SEis2zPSpappjVWt2lOwqaNXNJ0Rxfn0uJZ3GuWbeQC9ndkdWgymmb8MkXsSRhGA00gmS1542w'
+
+};;
