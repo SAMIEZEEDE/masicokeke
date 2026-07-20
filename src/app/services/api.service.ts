ebdd48025b0d215b8f3578bb7f2de385f7d4890a
sambulo <sambulo.ntshalintshali@neutrinos.com>
2026-07-20 12:54:29 +0200

    the correct order endpoint added

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class APIService {
- private apiUrl = environment.apiUrl;
+ private apiUrl = `${environment.apiUrl}/api/orders`;
   constructor(private http:HttpClient) { }
   pass(data:any){
     console.log(data)
